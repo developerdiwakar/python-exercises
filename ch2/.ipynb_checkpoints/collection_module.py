31fe2e4237083a7d78ab962436ae9a2bec8ac368
Diwakar Kumar <diwakar.kumar@umotif.com>
2022-09-21 19:58:06 +0530

    Collection module introduction completed

--- ch2/.ipynb_checkpoints/collection_module.py
+++ ch2/.ipynb_checkpoints/collection_module.py
@@ -1,4 +1,5 @@
-from collections import namedtuple
+from collections import ChainMap, defaultdict, namedtuple
+from enum import Enum
 '''
 namedtuple() => Factory function for creating tuple subclasses with named fields
 '''
@@ -15,5 +16,54 @@ print(vision2.middle)
 '''
 The defaultdict data type allows you to avoid checking if a key is in a dictionary by simply inserting it for you on your first access attempt, with a default value whose type you pass on creation.
 '''
+# For example
+d = {}
+d['age'] = d.get('age', 0) + 1 # age not there, we get 0 + 1
+print(d)
+# Output = {'age': 1}
+# We can write the above code like this also:
+dd = defaultdict(int) # int is the default type (0 the value)
+dd['age'] += 1 # short for dd['age'] = dd['age'] + 1
+print(dd)
+# Output = defaultdict(<class 'int'>, {'age': 1})
 
+'''
+ChainMap is an extremely nice data type which was introduced in Python 3.3. It behaves like a normal dictionary but according to the Python documentation: "is provided for quickly linking a number of mappings so they can be treated as a single unit"
+'''
+default_connection = { 'host': 'localhost', 'port': 4567}
+connection = { 'port': 5678 }
+conn = ChainMap(connection, default_connection) # Map creation
+print(conn['port']) # port is found in the first dict
+print(conn['host']) # host is found in the second dict
+
+print(conn.maps) # We can see the mapping the objects
+# Output = [{'port': 5678}, {'host': 'localhost', 'port': 4567}]
+conn['host'] = 'test.local.io' # let's add host
+print(conn.maps)
+# Output = [{'port': 5678, 'host': 'test.local.io'}, {'host': 'localhost', 'port': 4567}]
+del conn['port'] # let's remove the port information
+print(conn.maps)
+# Output = [{'host': 'test.local.io'}, {'host': 'localhost', 'port': 4567}]
+print(dict(conn))
+# Output = {'host': 'test.local.io', 'port': 4567}
+'''
+Enums - An enumeration is a set of symbolic names (members) bound to unique, constant values. Within an enumeration, the members can be compared by identity, and the enumeration itself can be iterated over.
+'''
+# GREEN = 1
+# YELLOW = 2
+# RED = 4
+# TRAFFIC_LIGHTS = (GREEN, YELLOW, RED)
+# OR WITH A DICT
+# traffic_lights = { 'GREEN': 1, 'YELLOW': 2, 'RED': 4}
+class TrafficLight(Enum):
+    GREEN = 1
+    YELLOW = 2
+    RED = 4
+print(TrafficLight.GREEN)
+# Output = TrafficLight.GREEN
+print(repr()) # their repr has more information
+print(TrafficLight.GREEN.name, TrafficLight.GREEN.value)
+# Output = GREEN, 1
+print(TrafficLight(1))
+# Output = TrafficLight.GREEN
 
