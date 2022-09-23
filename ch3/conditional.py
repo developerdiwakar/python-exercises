16a8933455df74d442b49644d601264c419e9aea
Diwakar Kumar <diwakar.kumar@umotif.com>
2022-09-23 13:06:15 +0530

    chapter 3 initiated with conditional statement

---
+++ ch3/conditional.py
@@ -0,0 +1,13 @@
+'''
+Write a program to check greatest number between the three numbers.
+'''
+a = eval(input('enter the 1st number: '))
+b = eval(input('enter the 2nd number: '))
+c = eval(input('enter the 3rd number: '))
+
+if a > b and a > c:
+    print(f'{a} is greatest number')
+elif b > c:
+    print(f'{b} is greatest number')
+else:
+    print(f'{c} is greatest number')
